31ae9350e39fd2876282c5aba7366b07eb0c8a60
Ann <apelsann@ya.ru>
2016-11-17 12:51:34 +0300

    1711

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 var menu_btn = document.querySelector('.page-header__btn');
 var menu_list = document.querySelector('.main-nav__list');
 var header = document.querySelector('.page-header');
-menu_btn.addEventListener('click', function(event)) {
+var logo = document.querySelector('.logo--header');
+
+
+menu_btn.addEventListener('click', function(event) {
 	event.preventDefault();
-	header.classList.toggle('page-header__menu-open');
+	header.classList.toggle('page-header--open');
+	menu_btn.classList.toggle('page-header__btn--close');
+	logo.classList.toggle('logo--header-open');
 	menu_list.classList.toggle('main-nav__list-show');
 });
